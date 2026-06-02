a3e24f274ad69ad3593b6e7c61b7ee3fdd173b5e
NadaAhme <nadaahmed33470@gmail.com>
2026-06-02 06:20:14 +0300

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`h-full antialiased`}
+      style={{ scrollBehavior: "auto" }}
     >
       <body className="min-h-full flex flex-col">
         <Navbar/>
