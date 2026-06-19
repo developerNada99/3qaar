b02dacccd1bf36a7558225435682c7439713fa5a
NadaAhme <nadaahmed33470@gmail.com>
2026-06-19 07:07:32 +0300

    add santiy

--- app/actions/getLandOne.ts
+++
@@ -1,6 +0,0 @@
-import { client } from "@/sanity/lib/client";
-import { LAND_ONE_QUERY } from "@/sanity/lib/queries";
-
-export async function getLandOne() {
-  return await client.fetch(LAND_ONE_QUERY);
-}
